40c4e61e902cd250886ff40a1ea2e1bc1365dd6d
Mahfuz Zayn <mahfuzzayn8@gmail.com>
2025-12-23 23:11:57 +0600

    Update in HeroSection: Added Clamp sizes elements, Added Iframe videos, Menus etc

--- src/components/modules/home/HeroSection.tsx
+++ src/components/modules/home/HeroSection.tsx
@@ -1,11 +1,25 @@
+"use client"
+
 import Image from "next/image";
 import Link from "next/link";
 import logoImg from "@/assets/images/logo.png"
-import { ArrowDown, Star } from "lucide-react";
+import { ArrowDown, Play, Star } from "lucide-react";
 import { Separator } from "@/components/ui/separator";
+import heroPoster from "@/assets/images/hero_poster.jpg"
 import clutchLogo from "@/assets/images/clutch-logo.png"
+import playImg from "@/assets/images/play.png"
+import clickToPlay from "@/assets/images/click-to-play.png"
+import { useRef, useState } from "react";
+import clsx from "clsx";
 
 const HeroSection = () => {
+  const [play, setPlay] = useState<boolean>(false);
+  const autoPlayerRef = useRef<HTMLIFrameElement | null>(null);
+
+  const videoSrc = play
+    ? "https://player.vimeo.com/video/823892274?h=fc36315143&autoplay=1"
+    : "https://player.vimeo.com/video/823892274?h=fc36315143";
+
   const menuLinks = [
     {
       label: "About Us",
@@ -25,12 +39,53 @@ const HeroSection = () => {
     },
   ]
 
+  const heroLinks = [
+    {
+      label: "Home",
+      href: "/"
+    },
+    {
+      label: "Our Services",
+      href: "/services"
+    },
+    {
+      label: "About Us",
+      href: "/about-us"
+    },
+    {
+      label: "Portfolio",
+      href: "/portfolio"
+    },
+    {
+      label: "Reviews",
+      href: "/reviews"
+    },
+    {
+      label: "Contact Us",
+      href: "/contact-us"
+    }
+  ]
+
+  const handlePlay = () => {
+    setPlay(true)
+
+    if (autoPlayerRef.current) {
+      autoPlayerRef.current.remove();
+    }
+  }
+
   return (
     <section className="hero-section p-3 h-screen">
-      <div className="bg-gray-200 px-[19px] pt-2 pb-[30px] rounded-[33px] overflow-hidden relative">
-        <div className="relative z-20">
+      <div className="h-full bg-gray-200 px-[19px] pt-2 pb-[30px] rounded-[33px] overflow-hidden relative">
+        {/* Hero Video */}
+        <video className="h-full w-full absolute z-10 inset-0 pointer-event-none object-cover object-center" poster={heroPoster.src} autoPlay muted loop>
+          <source src="./hero.mp4" type="video/mp4"></source>
+        </video>
+
+        {/* Hero Content */}
+        <div className="flex flex-col justify-between h-full relative z-20">
           {/* Hero Header */}
-          <header>
+          <header className="hero-header">
             <div className="flex mt-[15px]">
 
               <Link href="/" className="flex items-center w-[240px] gap-[9px]">
@@ -38,7 +93,7 @@ const HeroSection = () => {
                 <h4 className='text-lg font-bold text-white'>Awsmd</h4>
               </Link>
 
-              <ul className="bg-white text-black rounded-[33px] flex px-9 mx-auto">
+              <ul className="hidden lg:flex bg-white text-black rounded-[33px] px-9 mx-auto">
                 {menuLinks.map(link => (
                   <li key={link.href} className="px-5 py-[16px]">
                     <Link href={link.href} className="text-[15px] font-medium">{link.label}</Link>
@@ -49,12 +104,11 @@ const HeroSection = () => {
             </div>
           </header>
 
-          {/* Hero Content */}
-          <div className="pt-[25px] px-5">
-            <div className="hero-content flex justify-between text-white">
-              {/* Left Content */}
+          <div className="hero-content flex justify-between pt-[25px] px-5">
+            {/* Left Content */}
+            <div className="flex justify-between text-white">
               <div>
-                <div className="font-freigeist-cons text-[120px] tracking-tighter leading-[110px] pb-[25px] uppercase">
+                <div className="font-freigeist-cons text-[clamp(4.5rem,10vw,10rem)] tracking-tighter leading-[clamp(4.5rem,10vw,8rem)] pb-[25px] uppercase">
                   <h1>We create</h1>
                   <div className="flex items-center gap-4">
                     <div className="cursor-pointer w-[70px] h-[70px] flex items-center justify-center rounded-full bg-white text-black">
@@ -67,7 +121,7 @@ const HeroSection = () => {
                   </div>
                   <div>
                     <h1 className="font-bold">Design
-                      <span className="h-[100px] w-[20px] relative top-2 left-2 bg-white inline-block animate-pulse"></span>
+                      <span className="h-[clamp(4rem,8vw,8rem)] w-[clamp(0.5rem,5vw,1.3rem)] relative top-2 left-2 bg-white inline-block animate-pulse duration-100"></span>
                     </h1>
                   </div>
                 </div>
@@ -104,13 +158,49 @@ const HeroSection = () => {
 
               </div>
             </div>
+
+            {/* Right Content */}
+            <div className="space-y-10">
+              {/* Links */}
+              <div className="hidden lg:block space-y-[18px]">
+                {
+                  heroLinks.map(link => (
+                    <li key={link.href} className="list-none text-right">
+                      <Link href={link.href} className="font-plus-jakarta-sans text-[15px] hover:text-gray-500 transition duration-300 font-medium">{link.label}</Link>
+                    </li>
+                  ))
+                }
+              </div>
+
+              {/* Video */}
+              <div>
+                <button className={clsx("relative right-6 top-6 cursor-pointer bg-white/50 h-[clamp(60px,5vw,90px)] w-[clamp(60px,5vw,90px)] flex justify-center items-center rounded-full", { "hidden": play && play })} onClick={handlePlay}>
+                  <Image src={clickToPlay} alt="Click to Play" className="h-[138%] min-w-[138%] animate-spin" style={{ animationDuration: "10s" }} />
+                  <Image src={playImg} alt="Play Icon" className="absolute" />
+                </button>
+
+                {/* Auto Player Video */}
+                <iframe
+                  ref={autoPlayerRef}
+                  className="hero-video-autoplay h-[230px] w-[405px] rounded-[20px]"
+                  title="AWSMD Showreel"
+                  src="https://player.vimeo.com/video/823892274?h=fc36315143&autoplay=1&loop=1&muted=1&background=1"
+                  allow="autoplay; fullscreen; picture-in-picture"
+                  allowFullScreen
+                ></iframe>
+
+                {/* Main Player Video */}
+                <iframe
+                  className={clsx("hero-video-autoplay h-[230px] w-[405px] rounded-[20px]", { "hidden": !autoPlayerRef.current && !play })}
+                  title="AWSMD Showreel"
+                  src={videoSrc}
+                  allow="autoplay; fullscreen; picture-in-picture"
+                  allowFullScreen
+                ></iframe>
+              </div>
+            </div>
           </div>
         </div>
-
-        {/* Hero Video */}
-        <video className="absolute z-10 top-0 left-0 pointer-event-none" height="100%" width="100%" autoPlay muted loop>
-          <source src="./hero.mp4" type="video/mp4"></source>
-        </video>
       </div>
     </section>
   );
